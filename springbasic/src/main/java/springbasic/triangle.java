760e114b1377127b88dc3e6e929f3f50aeae2411
Aanuragkumarsrivastava <42467453+Aanuragkumarsrivastava@users.noreply.github.com>
2020-08-24 15:43:49 +0530

    uses constructor injection

--- springbasic/src/main/java/springbasic/triangle.java
+++ springbasic/src/main/java/springbasic/triangle.java
@@ -3,18 +3,39 @@ package springbasic;
 public class triangle
 {
 	private String type;
+	private int height;
+	
+	public triangle(String type)
+	{
+	this.type=type;	
+	}
+	
+	public triangle(String type,int height)
+	{
+		this.type=type;
+		this.height=height;
+	}
 	
 	public String getType() {
 		return type;
 	}
+	
+	
 
-	public void setType(String type) {
-		this.type = type;
+//	public void setType(String type) {
+//		this.type = type;
+//	}
+	public int getHeight() {
+		return height;
 	}
 	
+	
+
+
 	public void showprogress()
 	{
-		System.out.println(getType()+" is triange is drawing");
+		System.out.println(getType()+" is drawing having height "+getHeight());
 	}
+
 	
 }
